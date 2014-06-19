7e2ef672706980c83fc9e163a375992b380d24ab
Kuniaki Shimizu <kun432@8d1w.com>
2014-06-19 14:27:21 +0900

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'vagrant-digitalocean-docker'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures vagrant-digitalocean-docker'
+long_description 'Installs/Configures vagrant-digitalocean-docker'
+version          '0.1.0'
+
